a42d3f72dd0ed91edbb2d5aa842e2c70e45f3725
Dmitrijs Naudzuns <dmitrijsnaudzuns@gmail.com>
2023-06-01 17:54:41 -0400

    init

---
+++ TSandCompProj/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Card from '@/components/Card.vue';
+import type {Id} from '../types'
+
+const cardId:Id = {IdA:'A',IdB:1}
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Video Game Sorter</h1>
+    <Card :title="'Test'" :content="'Passing this as a prop'" :id=cardId />
+    
+  </main>
+</template>
